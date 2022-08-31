24beb0a3f398cb476a2822845d671d47c04ed87a
Agustin Juan <agustinchoslr@gmail.com>
2022-08-31 14:46:35 -0300

    todos los botones creados

--- transformando-a-angular/portargfolio/src/app/componentes/proyectos-logros/proyectos-logros.component.ts
+++ transformando-a-angular/portargfolio/src/app/componentes/proyectos-logros/proyectos-logros.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Proyecto } from 'src/app/servicios/proyecto';
 import { ProyectoService } from 'src/app/servicios/proyecto.service';
 
@@ -9,7 +10,9 @@ import { ProyectoService } from 'src/app/servicios/proyecto.service';
   styleUrls: ['./proyectos-logros.component.css']
 })
 export class ProyectosLogrosComponent implements OnInit {
-  public proyectos: Proyecto[] | undefined;
+  public proyectos: Proyecto[] | undefined;  
+  public editProyecto: Proyecto | undefined;  
+  public deleteProyecto: Proyecto | undefined;
 
   constructor(private proyectoService: ProyectoService) {}
 
@@ -28,4 +31,65 @@ export class ProyectosLogrosComponent implements OnInit {
     }
     );
   }
+
+  
+  
+  public onOpenModal(proyecto: Proyecto, mode:string): void {
+    const container = document.getElementById('proyectos-button-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {      
+    button.setAttribute('data-target', '#addProyectoModal');
+    }
+    if (mode === 'edit') {      
+      this.editProyecto = proyecto;
+      button.setAttribute('data-target', '#updateProyectoModal');
+      }
+      if (mode === 'delete') {     
+        this.deleteProyecto = proyecto; 
+        button.setAttribute('data-target', '#deleteProyectoModal');
+        }
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onAddProyecto(addform: NgForm): void  {
+    document.getElementById('proyecto-button-close')?.click();
+    this.proyectoService.addProyecto(addform.value).subscribe(
+      (response : Proyecto) => {
+        console.log(response);
+        this.getProyectos();
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onUpdateProyecto(proyecto: Proyecto): void  {
+    this.proyectoService.updateProyecto(proyecto).subscribe(
+      (response : Proyecto) => {
+        console.log(response);
+        this.getProyectos();
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteProyecto(proyectoId: number): void  {
+    this.proyectoService.deleteProyecto(proyectoId).subscribe(
+      (response : void) => {
+        console.log(response);
+        this.getProyectos();
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
 }
