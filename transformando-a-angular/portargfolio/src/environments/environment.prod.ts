3224ac2b08bf7897639cdf97d0b713190b787f17
agustinchoslr <agustinchoslr@gmail.com>
2022-12-29 20:45:21 -0300

    changing from heroku to koyeb configs

--- transformando-a-angular/portargfolio/src/environments/environment.prod.ts
+++ transformando-a-angular/portargfolio/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   // apiBaseUrl: 'https://portargfolio.herokuapp.com',
   // apiAuthUrl: 'https://portargfolio.herokuapp.com/auth/'
   
-  apiBaseUrl: 'portfolio-agustinchoslr.koyeb.app/',
-  apiAuthUrl: 'portfolio-agustinchoslr.koyeb.app/auth/'
+  apiBaseUrl: 'https://portfolio-agustinchoslr.koyeb.app/',
+  apiAuthUrl: 'https://portfolio-agustinchoslr.koyeb.app/auth/'
 };
