40d157cc2e722413f2595af12369f46669355446
agustinchoslr <agustinchoslr@gmail.com>
2022-12-29 20:41:26 -0300

    cambiando de sv de back a koyeb

--- transformando-a-angular/portargfolio/src/environments/environment.prod.ts
+++ transformando-a-angular/portargfolio/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://portargfolio.herokuapp.com',
-  apiAuthUrl: 'https://portargfolio.herokuapp.com/auth/'
+  // apiBaseUrl: 'https://portargfolio.herokuapp.com',
+  // apiAuthUrl: 'https://portargfolio.herokuapp.com/auth/'
+  
+  apiBaseUrl: 'portfolio-agustinchoslr.koyeb.app/',
+  apiAuthUrl: 'portfolio-agustinchoslr.koyeb.app/auth/'
 };
--- transformando-a-angular/portargfolio/src/environments/environment.ts
+++ transformando-a-angular/portargfolio/src/environments/environment.ts
@@ -4,8 +4,11 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://portargfolio.herokuapp.com',
-  apiAuthUrl: 'https://portargfolio.herokuapp.com/auth/'
+  // apiBaseUrl: 'https://portargfolio.herokuapp.com',
+  // apiAuthUrl: 'https://portargfolio.herokuapp.com/auth/'
+  
+  apiBaseUrl: 'portfolio-agustinchoslr.koyeb.app/',
+  apiAuthUrl: 'portfolio-agustinchoslr.koyeb.app/auth/'
 };
 
 /*
