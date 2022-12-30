159605ec54cfb0d94a24c4227d3af8d81887b756
agustinchoslr <agustinchoslr@gmail.com>
2022-12-29 22:13:24 -0300

    changing from heroku to koyeb cause newherokuterms

--- transformando-a-angular/portargfolio/src/environments/environment.ts
+++ transformando-a-angular/portargfolio/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   // apiBaseUrl: 'https://portargfolio.herokuapp.com',
   // apiAuthUrl: 'https://portargfolio.herokuapp.com/auth/'
   
-  apiBaseUrl: 'https://portfolio-agustinchoslr.koyeb.app/',
+  apiBaseUrl: 'https://portfolio-agustinchoslr.koyeb.app',
   apiAuthUrl: 'https://portfolio-agustinchoslr.koyeb.app/auth/'
 };
 
